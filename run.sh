e4e9cefcb00aedd751723573d81da7124c5ba37a
Adrian Bielec <adrian.bielec@arianelabs.com>
2025-05-14 08:28:55 +0200

    Improve select

--- run.sh
+++ run.sh
@@ -66,45 +66,25 @@ esac
 read -p "Do you want to add a scheduler? (y/n): " add_scheduler
 
 if [[ "$add_scheduler" =~ ^[Yy]$ ]]; then
-options=("Every 5m", "Every 15m", "Every 30m", "Every 45m", "Every hour" "Every 2 hours" "Every 3 hours" "Every 4 hours", "Custom pattern")
+options=("Hourly" "Every half hour" "Every quarter" "Custom pattern")
 function show_menu() {
-  echo "Startup frequency"
+  echo "How often do you want to run it?"
   PS3="Enter your choice (1-5): "
   select choice in "${options[@]}"; do
     case $REPLY in
       1)
-        frequency="5m"
-        break
-        ;;
-      2)
-        frequency="15m"
-        break
-        ;;
-      3)
-        frequency="30m"
-        break
-        ;;
-      4)
-        frequency="45m"
-        break
-        ;;
-      5)
         frequency=1
         break
         ;;
-      6)
-        frequency=2
+      2)
+        frequency="30m"
         break
         ;;
-      7)
-        frequency=3
+      3)
+        frequency="15m"
         break
         ;;
-      8)
-        frequency=4
-        break
-        ;;
-      9)
+      4)
         echo "Enter the cron pattern (e.g., '0 * * * *'): (default)[*/15 * * * * *]"
         IFS= read -r cron_pattern
         cron_pattern=${cron_pattern:-"*/15 * * * * *"}
@@ -127,8 +107,34 @@ if [[ -n "$cron_pattern" ]]; then
   scheduler_arg="--scheduler=$cron_pattern"
 fi
 
-  # Ask for stop-after argument
-  read -p "Enter the stop-after duration (e.g., '2w', '3d', '5h', '30m'): " stop_after_duration
+  echo "For how long you want the schedule to be active?"
+  PS3="Enter your choice (1-4): "
+  options_duration=("1 hour", "3 hours", "1 day", "I want to provide interval string")
+  select choice in "${options_duration[@]}"; do
+    case $REPLY in
+      1)
+        stop_after_duration="1h"
+        break
+        ;;
+      2)
+        stop_after_duration="3h"
+        break
+        ;;
+      3)
+        stop_after_duration="1d"
+        break
+        ;;
+      4)
+        read -p "Enter the stop-after duration (e.g., '2w', '3d', '5h', '30m'): " stop_after_duration
+        echo "Enter the cron pattern (e.g., '0 * * * *'): (default)[*/15 * * * * *]"
+        break
+        ;;
+      *)
+        echo "Invalid option. Please try again."
+        ;;
+    esac
+  done
+
 
   # Validate stop-after duration
   if [[ "$stop_after_duration" =~ ^[0-9]+[mhdw]$ ]]; then
